68a7c7c3b10fea6ecad9333c4fa3281ff33693aa
MRxTHUNDER <sahilchahal221@gmail.com>
2024-11-08 16:39:14 +0530

    calc app

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <div>
+    <h2> Welcome to MY CALC app</h2>
+    <CalApp
+      @button-clicked="handleButtonClick"
+      @operator-selected="handleOperatorSelected"
+      @calculation-completed="handleCalculationCompleted"
+    />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import CalApp from './components/CalApp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    CalApp,
+  },
+  methods: {
+    handleButtonClick(value) {
+      console.log('Button clicked:', value);
+    },
+    handleOperatorSelected(operator) {
+      console.log('Operator selected:', operator);
+    },
+    handleCalculationCompleted(result) {
+      console.log('Calculation completed:', result);
+    }
   }
 }
 </script>
